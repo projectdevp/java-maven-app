25e4da20235bc55330597aec05544518c64249e7
projectdevp <152896703+projectdevp@users.noreply.github.com>
2024-01-24 17:09:07 +0100

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -6,9 +6,9 @@ def buildJar() {
 def buildImage() {
     echo "building the docker image..."
     withCredentials([usernamePassword(credentialsId: 'dockerhub-credentials', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh 'docker build -t tawfiqnajib/java-maven-app:version 2.0 .'
+        sh 'docker build -t tawfiqnajib/java-maven-app:version2.0 .'
         sh "echo $PASS | docker login -u $USER --password-stdin"
-        sh 'docker push tawfiqnajib/java-maven-app:version 2.0'
+        sh 'docker push tawfiqnajib/java-maven-app:version2.0'
     }
 } 
 
