9befba2831a4e52fe22704533a774ed09f780256
projectdevp <152896703+projectdevp@users.noreply.github.com>
2024-01-27 20:33:25 +0100

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -1,3 +1,4 @@
+#!/usr/bin/env groovy
 def buildJar() {
     echo "building the application..."
     sh 'mvn package'
@@ -6,14 +7,30 @@ def buildJar() {
 def buildImage() {
     echo "building the docker image..."
     withCredentials([usernamePassword(credentialsId: 'dockerhub-credentials', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh 'docker build -t tawfiqnajib/java-maven-app:version2.0 .'
+        sh 'docker build -t tawfiqnajib/java-maven-app:jma-3.0 .'
         sh "echo $PASS | docker login -u $USER --password-stdin"
-        sh 'docker push tawfiqnajib/java-maven-app:version2.0'
+        sh 'docker push tawfiqnajib/java-maven-app:jma-3.0'
     }
-} 
+}
 
 def deployApp() {
-    echo 'deploying the application...'
-} 
+    echo 'Deploying the application...'
+
+    // Configuration des informations de connexion à votre instance EC2
+    def remoteUser = 'ec2-user'
+    def remoteHost = 'your-ec2-instance-ip'
+    def dockercmd= 'docker run -d -p 8080:8080 tawfiqnajib/java-maven-app:jma-3.0'
+
+    // Définition des clés SSH à utiliser (doit être configuré dans Jenkins sous "Manage Jenkins" -> "Manage Credentials")
+    def sshCredentialsId = 'your-ssh-credentials-id'
+
+    // Utilisation du plugin SSH Agent pour gérer les clés SSH
+    sshagent(['your-ssh-credentials-id']) {
+        // Commandes SSH pour déployer l'application sur votre instance EC2
+        sh "ssh -o StrictHostKeyChecking=no ${remoteUser}@${remoteHost} ${dockercmd}"
+
+    }
+}
+
 
 return this
